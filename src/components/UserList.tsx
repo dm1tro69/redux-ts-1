79ad42e3eb18a7444ad9a657458697bbe6fcfed1
dm1tro69 <dimolg22@gmail.com>
2021-07-04 16:29:59 +0300

    add dispatch with thunk

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -1,13 +1,30 @@
-import React, {FC} from 'react';
-import {useSelector} from "react-redux";
+import React, {FC, useEffect} from 'react';
+import {useDispatch, useSelector} from "react-redux";
 import {useTypedSelector} from "../hooks/useTypedSelector";
+import {fetchUsers} from "../store/action-creator/user";
 
 const UserList: FC = () => {
     const {users, error, loading} = useTypedSelector((state) => state.users)
+   const dispatch = useDispatch()
+
+    useEffect(()=> {
+       dispatch( fetchUsers())
+    }, [])
+    
+    if (loading) {
+        return <h1>Loading...</h1>
+    }
+    if (error) {
+        return <h1>{error}</h1>
+    }
 
     return (
         <div>
-
+            {users.map((user)=> {
+                return (
+                    <div key={user.id}>{user.name}</div>
+                )
+            })}
         </div>
     );
 };
